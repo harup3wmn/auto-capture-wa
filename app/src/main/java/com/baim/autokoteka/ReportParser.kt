7c5726e8bb0869a22b969cd0278fa36865b039a6
Ibrahim Achmad Yamin <ibrahimachmadyamin14@gmail.com>
2026-06-18 16:39:59 +0900

    Make ReportParser regex more flexible to prevent dropping reports with spacing typos

--- app/src/main/java/com/baim/autokoteka/ReportParser.kt
+++ app/src/main/java/com/baim/autokoteka/ReportParser.kt
@@ -4,12 +4,12 @@ import android.util.Log
 
 object ReportParser {
 
-    private val regexTanggal = Regex("𝙃𝘼𝙍𝙄 / 𝙏𝘼𝙉𝙂𝙂𝘼𝙇 : (.*)")
-    private val regexTHariIni = Regex("PEROLEHAN PENEBANGAN SEJUMLAH = (\\d+)")
-    private val regexPK = Regex("1\\. Penebangan pohon 5 - 20 Cm = (\\d+)")
-    private val regexPS = Regex("2\\. Penebangan pohon 20 - 30 Cm = (\\d+)")
-    private val regexPB1 = Regex("3\\. Penebangan pohon 30 - 50 Cm = (\\d+)")
-    private val regexPB2 = Regex("4\\. Penebangan pohon 50 > Cm = (\\d+)")
+    private val regexTanggal = Regex("𝙃𝘼𝙍𝙄 / 𝙏𝘼𝙉𝙂𝙂𝘼𝙇\\s*:\\s*(.*)")
+    private val regexTHariIni = Regex("PEROLEHAN PENEBANGAN SEJUMLAH\\s*=\\s*(\\d+)")
+    private val regexPK = Regex("1\\.\\s*Penebangan pohon 5\\s*-\\s*20 Cm\\s*=\\s*(\\d+)")
+    private val regexPS = Regex("2\\.\\s*Penebangan pohon 20\\s*-\\s*30 Cm\\s*=\\s*(\\d+)")
+    private val regexPB1 = Regex("3\\.\\s*Penebangan pohon 30\\s*-\\s*50 Cm\\s*=\\s*(\\d+)")
+    private val regexPB2 = Regex("4\\.\\s*Penebangan pohon 50\\s*>\\s*Cm\\s*=\\s*(\\d+)")
 
     data class ParsedData(
         val isYalimo: Boolean,
